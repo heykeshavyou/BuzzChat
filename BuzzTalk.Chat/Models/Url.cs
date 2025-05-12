6109ce845d283c3d81156c835f35e54d491e79d7
Keshav <keshav210305@gmail.com>
2025-05-12 21:44:17 +0530

    changes

--- BuzzTalk.Chat/Models/Url.cs
+++ BuzzTalk.Chat/Models/Url.cs
@@ -5,10 +5,10 @@ namespace BuzzTalk.Chat.Models
     public class Url
     {
 
-        private const bool ISLOCALHOST = false;
-        private const bool ISLIVE = false;
+        private const bool ISLOCALHOST = true;
+        private const bool ISLIVE = true;
         public static string liveUrl = "http://heykeshav-001-site1.ntempurl.com/";
-        public static string LocalUrl = ISLOCALHOST?  "http://192.168.1.13:45455/": "https://localhost:7059/";
+        public static string LocalUrl = ISLOCALHOST? "https://192.168.1.15:45457/" : "https://localhost:7059/";
 
         public static string baseUrl = ISLIVE ? liveUrl : LocalUrl;
 
