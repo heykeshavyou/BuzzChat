c33068b099431851021bbb75c4f0d0c227cf14c0
Keshav <keshav210305@gmail.com>
2026-01-03 20:18:34 +0530

    Delete BuzzTalk.Chat directory

--- BuzzTalk.Chat/Models/GetmessageModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BuzzTalk.Chat.Models
-{
-    public class GetmessageModel
-    {
-        public int FromId { get; set; }
-        public int ToId { get; set; }
-    }
-}
--- BuzzTalk.Chat/Models/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BuzzTalk.Chat.Models
-{
-    public class LoginModel
-    {
-        public string username { get; set; } = null!;
-        public string password { get; set; } = null!;
-    }
-}
